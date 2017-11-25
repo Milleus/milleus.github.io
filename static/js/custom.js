cfb1337a9163dae0eed6dce7872013084524a4cc
Dave Quah <sdquah88@gmail.com>
2017-11-26 00:29:20 +0800

    [Dave] add typing effect, update about content, change back to yellow color

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,9 +1,47 @@
-  // Initialize collapse button
-  $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  //$('.collapsible').collapsible();
+/* spells on global cooldown */
+const words = ['software engineer.', 'learner.', 'tech enthusiast.'];
+let wordIndex = 0;
 
-$('#slide-out > li > a').on('click', function() {
+/* type and wipe */
+$(document).ready(function () {
+  startType(words[wordIndex], 0);
+});
+
+function startType(word, charIndex) {
+  if (charIndex < word.length) {
+    document.getElementById('typity').innerHTML += word.charAt(charIndex);
+    charIndex++;
+    setTimeout(function () {
+      startType(word, charIndex);
+    }, 50);
+  } else {
+    startWipe();
+  }
+}
+
+function startWipe() {
+  wordIndex = wordIndex < words.length - 1 ? wordIndex + 1 : 0;
+
+  setTimeout(function () {
+    $('mark').addClass('highlighted')
+  }, 1000);
+
+  setTimeout(function () {
+    $('mark').removeClass('highlighted').text('');
+  }, 2000);
+
+  setTimeout(function () {
+    startType(words[wordIndex], 0);
+  }, 2000);
+}
+
+/* side navigation */
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
+
+/* slide animation */
+$('#slide-out > li > a').on('click', function () {
   const href = $(this).attr('href').replace(/[\/]+/g, '');
   $('html, body').animate({
     scrollTop: $(href).offset().top
