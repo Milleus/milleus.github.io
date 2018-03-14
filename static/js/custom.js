39445145fd67e77b3499f4258ffaef8120751387
Dave Quah <sdquah88@gmail.com>
2018-03-14 20:07:42 +0800

    [Dave] clean up styling and make adjustments for mobile

--- static/js/custom.js
+++ static/js/custom.js
@@ -39,7 +39,10 @@ function startWipe() {
 }
 
 // side navigation
-$(".button-collapse").sideNav();
+$(".button-collapse").sideNav({
+  closeOnClick: true,
+  draggable: true
+});
 // Initialize collapsible (uncomment the line below if you use the dropdown variation)
 //$('.collapsible').collapsible();
 
